65202579320c5b83f8e66568192af1ca18f5fbfe
Klaus Kazlauskas <klausmachado@gmail.com>
2018-10-17 21:45:18 -0300

    chore: create ng-rapi library

---
+++ projects/ng-rapi/src/lib/ng-rapi.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgRapiComponent } from './ng-rapi.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [NgRapiComponent],
+  exports: [NgRapiComponent]
+})
+export class NgRapiModule { }
---
+++ projects/ng-rapi/src/public_api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ng-rapi
+ */
+
+export * from './lib/ng-rapi.module';
