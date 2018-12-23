248780e792d4c71af47e9af915cc93554493b1a1
Klaus Kazlauskas <klausmachado@gmail.com>
2018-12-23 16:43:17 -0200

    refactor(ng-rapi): remove unsued module

--- projects/ng-rapi/src/lib/api-builder.service.ts
+++ projects/ng-rapi/src/lib/api-builder.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Api } from './api';
-import { NgRapiModule } from './ng-rapi.module';
 
 @Injectable({
-  providedIn: NgRapiModule
+  providedIn: 'root'
 })
 export class ApiBuilderService {
 
--- projects/ng-rapi/src/public_api.ts
+++ projects/ng-rapi/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ng-rapi
  */
 
-export * from './lib/ng-rapi.module';
 export * from './lib/api-builder.service';
 export * from './lib/api';
